cdbc9d5a1b3a17c31e9a912d206f8ea42dac83a4
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-12 01:22:35 +0000

    final

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 characters = "abcdefghijklmnopqrstuvwxyz"
+characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+characters += "0123456789"
+characters += "!@#$%^&*()_+-=[]{}|;':\"<>?"
+characters += "\"\\"
 
 character_count = len(characters)
 
 print("Supported Characters:\n" + characters + "\n")
 
 def encrypt_character(plain, key):
+  
   key_code = characters.index(key)
   plain_code = characters.index(plain)
 
@@ -19,27 +24,36 @@ def encrypt(plain, key):
     key_index = plain_index % len(key)
     key_character = key[key_index]
     
-    cipher_character = encrypt_character(plain_character, key_character)
+    cipher_character = encrypt_character(key_character, plain_character)
     cipher += cipher_character
+  
   return cipher 
 def invert_character(character):
   character_code = characters.index(character)
-  inverted_character = (character_count - character_code) % character_count
-  inverted_character = characters[inverted_character]
-return inverted_character
+  inverted_code = (character_count - character_code) % character_count
+  inverted_character = characters[inverted_code]
 
+  return inverted_character
 def invert(text):
   inverted_text = ""
 
-for character in text:
-  inverted_text += invert_character(character)
+  for character in text:
+    inverted_text += invert_character(character)
   return inverted_text
 
-plaintext = "secretmessage"
-keytext = "key"
-ciphertext = encrypt(plaintext, keytext)
+while True: 
+  plaintext = input("Message: ")
+  keytext = input("Password: ")
+  
+  encrypted = plaintext.startswith("!")
+  
+  if encrypted:
+    plaintext = plaintext[1:]
+    keytext = invert(keytext)
+  ciphertext = encrypt(plaintext, keytext)
+  
+  if not encrypted:
+    ciphertext = "!" + ciphertext
 
-print("Message: " + plaintext)
-print("Password: " + keytext)
-print("Output: " + ciphertext)
-
+  print("Output: " + ciphertext)
+  print()
